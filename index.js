5cb14eb5f8cc9d420492490e9611094786af5f94
HuaMa <knid855jieh8d56201@gmail.com>
2022-11-01 18:23:58 +0800

    no message

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 
+// 修改路徑
 const url = path.join('C:/Users/yisiang.chen/Downloads/test01/');
+// 留下的名稱規則
+const regexp = /\[[0-9][0-9]\]/;
 
 fs.readdir(url, 'utf8', (err, fileList) => {
     if (err) {
-        throw err;
+        console.log(err);
+        return;
     };
 
-    fileList.forEach((item, index) => {
+    fileList.forEach((item) => {
         let length = item.split('.').length;
 
-        // 获取文件后缀名
         let type = '.' + item.split('.')[length - 1]; 
-       
         let oldName = item;
 
-        const regexp = /\[[0-9][0-9]\]/;
-        let newName = oldName.match(regexp)[0];
-        newName = newName.replace("[", "第").replace("]", "集") + type;
-        console.log(newName);
-        console.log(url + item);
-        // let newName = index + '.jpg';
+        const _name = oldName.match(regexp);
 
-        fs.rename(url + item, url + newName, (err) => {
-            console.log(err);
-            throw err;
-        });
+        if(_name != null) {
+            let newName = _name[0];
+            newName = newName.replace("[", "第").replace("]", "集") + type;
+            fs.rename(url + item, url + newName, () => {});
+        }
     })
 })
